61a56d176dc5b40d987b01bba6597f11143306c3
Patrick Harrison <jahman@bellatlantic.net>
2019-07-31 17:03:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,8 +17,8 @@ end
 def age_appropriate_birthday (birthday_kids)
   birthday_kids.each do |kids_name, age|
   if age > 12
-else 
-  puts "You are too old for this"
+  else 
+    puts "You are too old for this"
 
  end
  end
