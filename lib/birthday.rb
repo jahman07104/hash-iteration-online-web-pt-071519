d1e80341e97801c9e967de13deeece4d2858a863
Patrick Harrison <jahman@bellatlantic.net>
2019-07-31 15:54:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if birthday_kids  , 12
+    if birthday_kids < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     
   end
