0e0a08d77ba561faa1f6c1658fc1637222e28fbb
Patrick Harrison <jahman@bellatlantic.net>
2019-07-31 15:40:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,3 +16,8 @@ end
  
 winner# add the code snippet here!
 end
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
