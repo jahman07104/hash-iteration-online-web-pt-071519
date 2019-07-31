d1026501682b3c4b096a08e0e70af42849936e96
Patrick Harrison <jahman@bellatlantic.net>
2019-07-31 15:48:12 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -22,7 +22,9 @@ end
 #   "Amanda" => 27
 # }
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do 
+    |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} 
+      years old!"
   end
 end
